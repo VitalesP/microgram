2336dc4e63784aff8e5ff5cdf184225cb9c6691b
Vital <vitalesperegudov@gmail.com>
2021-01-21 16:44:05 +0600

    homework51: lesson02: create project

---
+++ src/main/java/com/microgram/home51lesson02/Home51lesson02Application.java
@@ -0,0 +1,13 @@
+package com.microgram.home51lesson02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Home51lesson02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Home51lesson02Application.class, args);
+    }
+
+}
