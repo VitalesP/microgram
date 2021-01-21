b62d86deceb1cc74474715288c4f0d904a001002
Vital <vitalesperegudov@gmail.com>
2021-01-21 19:02:12 +0600

    homework51: Task0: forgotten files

---
+++ src/test/java/com/microgram/home51lesson02/Home51lesson02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microgram.home51lesson02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Home51lesson02ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
