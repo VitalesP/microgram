51df49cc958f62a392708a4dbd8249bfaed4e005
Vital <vitalesperegudov@gmail.com>
2021-01-27 17:33:22 +0600

    homework52: Task2: create controllers for all models

---
+++ src/main/java/com/microgram/home51lesson02/controller/CommentController.java
@@ -0,0 +1,7 @@
+package com.microgram.home51lesson02.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class CommentController {
+}
---
+++ src/main/java/com/microgram/home51lesson02/service/CommentService.java
@@ -0,0 +1,9 @@
+package com.microgram.home51lesson02.service;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class CommentService {
+}
