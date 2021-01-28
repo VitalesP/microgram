689ecb55d3f79d86c12387fba7b05ec86d820f52
Vital <vitalesperegudov@gmail.com>
2021-01-28 15:54:35 +0600

    homework53:  create package annotations

--- src/main/java/com/microgram/home51lesson02/controller/PublicationController.java
+++ src/main/java/com/microgram/home51lesson02/controller/PublicationController.java
@@ -1,7 +1,27 @@
 package com.microgram.home51lesson02.controller;
 
-import org.springframework.stereotype.Controller;
+import com.microgram.home51lesson02.DTO.PublicationDto;
+import com.microgram.home51lesson02.service.PublicationService;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.annotations.ApiIgnore;
 
-@Controller
+@RestController
+@RequestMapping("/publication")
 public class PublicationController {
+    private final PublicationService publicationService;
+
+
+    public PublicationController(PublicationService publicationService) {
+        this.publicationService = publicationService;
+    }
+
+    @ApiPageable
+    @GetMapping
+    public Slice<PublicationDto> findPublication(@ApiIgnore Pageable pageable) {
+        return publicationService.findMovies(pageable);
+    }
 }
